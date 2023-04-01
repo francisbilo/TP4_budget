b3bd4d70afe16467cc3e4a68f2f26347f0cea81c
francisbilo <francis.bilodeau-lecomte@polymtl.ca>
2023-03-31 20:00:03 -0400

    Tried to fix script

--- myscript.py
+++ myscript.py
@@ -2,11 +2,13 @@ import os
 import sys
 
 
-def main(bad_hash, good_hash):
+def main():
+    bad_hash = 'c1a4be04b972b6c17db242fc37752ad517c29402'
+    good_hash = 'e4cfc6f77ebbe2e23550ddab682316ab4ce1c03c'
     os.system(f'git bisect start {bad_hash} {good_hash}')
     os.system('git bisect run python manage.py test')
     os.system('git bisect reset')
 
 
 if __name__ == "__main__":
-    main(sys.argv[1], sys.argv[2])
+    main()
